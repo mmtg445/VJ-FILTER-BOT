f6386167d5c88ead0fd2200c30b86a5fc98c1c71
mmtg445 <mmtg445@gmail.com>
2025-03-06 20:18:29 +0600

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ PICS = (environ.get('PICS', 'https://files.catbox.moe/cu8far.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7945551029 7822720438').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
